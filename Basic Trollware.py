6300fd573932ad6c2a10b73e521a68c247e3747b
CompAndCode <66060875+compandcode@users.noreply.github.com>
2020-11-08 14:54:53 +0000

    Add files via upload

---
+++ Basic Trollware.py
@@ -0,0 +1,12 @@
+import webbrowser #Library for interacting with the webbrowser.
+
+while True: #While the program is running.
+    webbrowser.open("https://www.google.com") #Open Google in a new tab.
+
+"""
+Basic trollware.
+
+DISCLAIMER: Use at your own risk.
+            I'm not responsible for what you do with this code.
+            You could get into trouble if you use it irresponsibly.
+""
